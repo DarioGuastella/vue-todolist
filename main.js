97eee559b3eb4e5bb648d29b002298d30f140f3c
DarioGuastella <jimmyguastella@gmail.com>
2023-11-17 18:14:31 +0100

    feat: stampa lista attività legge true o false

--- main.js
+++ main.js
@@ -2,40 +2,40 @@ const { createApp } = Vue
 //   Ogni attività sarà rappresentata da un oggetto con due proprietà:
 //   - text: una stringa che indica il testo del ToDo
 //   - done: un booleano (true/false) che indica se il ToDo è stato fatto oppure no
-  createApp({
+createApp({
     data() {
-      return [
-        {
-        text: "Lavare i piatti",
-        done: false
-      },
-      {
-        text: "Fare la spesa",
-        done: false
-      },
-      {
-        text: "Cambiare l'acqua al pesce",
-        done: true
-      },
-      {
-        text: "Risolvere il conflitto mondiale",
-        done: false
-      },
-      {
-        text: "Lavare la macchina",
-        done: false
-      },
-    ]
+        return {
+            toDoList: [
+                {
+                    text: "Lavare i piatti",
+                    done: false
+                },
+                {
+                    text: "Fare la spesa",
+                    done: false
+                },
+                {
+                    text: "Cambiare l'acqua al pesce",
+                    done: true
+                },
+                {
+                    text: "Risolvere il conflitto mondiale",
+                    done: false
+                },
+                {
+                    text: "Lavare la macchina",
+                    done: false
+                },
+            ]
+        }
     }
 
-  }).mount('#app')
+}).mount('#app')
 
 //   DESCRIZIONE:
 //   Creare una web application che mostri una lista di cose da fare.
 
-//   MILESTONE 1
-//   Stampare all'interno di una lista HTML un item per ogni ToDo, visualizzandone il testo.
-//   Se la proprietà "done" è uguale a true, visualizzare il testo del ToDo sbarrato.
+
 //   MILESTONE 2
 //   Visualizzare a fianco ad ogni item una "X": cliccando su di essa, il ToDo viene rimosso dalla lista.
 //   MILESTONE 3
