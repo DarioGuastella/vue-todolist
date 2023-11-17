949cb92713db5c380e70a236c9c6938207a6c085
DarioGuastella <jimmyguastella@gmail.com>
2023-11-17 17:59:07 +0100

    feat: inizia esercizio con array di oggetti

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 const { createApp } = Vue
-
+//   Ogni attività sarà rappresentata da un oggetto con due proprietà:
+//   - text: una stringa che indica il testo del ToDo
+//   - done: un booleano (true/false) che indica se il ToDo è stato fatto oppure no
   createApp({
     data() {
-      return {
-        count: 0
-      }
+      return [
+        {
+        text: "Lavare i piatti",
+        done: false
+      },
+      {
+        text: "Fare la spesa",
+        done: false
+      },
+      {
+        text: "Cambiare l'acqua al pesce",
+        done: true
+      },
+      {
+        text: "Risolvere il conflitto mondiale",
+        done: false
+      },
+      {
+        text: "Lavare la macchina",
+        done: false
+      },
+    ]
     }
+
   }).mount('#app')
+
+//   DESCRIZIONE:
+//   Creare una web application che mostri una lista di cose da fare.
+
+//   MILESTONE 1
+//   Stampare all'interno di una lista HTML un item per ogni ToDo, visualizzandone il testo.
+//   Se la proprietà "done" è uguale a true, visualizzare il testo del ToDo sbarrato.
+//   MILESTONE 2
+//   Visualizzare a fianco ad ogni item una "X": cliccando su di essa, il ToDo viene rimosso dalla lista.
+//   MILESTONE 3
+//   Predisporre un input testuale e un pulsante "aggiungi". Cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo oggetto ToDo, che quindi viene aggiunto alla lista dei ToDo.
